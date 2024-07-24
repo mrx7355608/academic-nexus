a46db8bebf49ce96abcf953403f74249c789fb0f
mrx7355608 <mrx7355608@gmail.com>
2024-07-24 14:03:29 -0700

    testing rate limiting on download route

--- tests/e2e/assessments.spec.js
+++ tests/e2e/assessments.spec.js
@@ -7,6 +7,7 @@ let request;
 let cookie =
     "connect.sid=s%3AUTZcVczCRrIkTQkIazxYWC3__piLaokh.sd5bTdnk3lF9Fg28bOddkYEuTolwHS7cKKTfhN%2BSzuA";
 let user;
+const id = "669c4d898170a78748d06eaf";
 
 describe("Assessments tests", () => {
     beforeAll(async () => {
@@ -46,8 +47,6 @@ describe("Assessments tests", () => {
     });
 
     describe("Testing Upvotes & Downvotes", () => {
-        const id = "669c4d898170a78748d06eaf";
-
         it("should upvote", async () => {
             const response = await request
                 .post(`/api/assessments/${id}/upvote`)
@@ -68,9 +67,39 @@ describe("Assessments tests", () => {
         });
     });
 
-    describe("Testing rate limits on /download-file/:id", () => {
-        it.todo("should validate password before downloading file");
-        it.todo("should return limit exceeded error after 3 tries");
+    describe("Testing /download-file/:id", () => {
+        it("should validate password before downloading file", async () => {
+            const response = await request
+                .post(`/api/assessments/download-file/${id}`)
+                .send({
+                    password: "wrong-password",
+                })
+                .expect(403);
+            expect(response.body.error).toBe("Incorrect password");
+        });
+
+        it("should return limit exceeded error after 3 tries", async () => {
+            // its 2 here, because a request is already made in the above test,
+            // so total request count becomes 3 when this test runs
+            for (let index = 0; index < 2; index++) {
+                await request
+                    .post(`/api/assessments/download-file/${id}`)
+                    .send({
+                        password: "wrong-password",
+                    });
+            }
+
+            const response = await request
+                .post(`/api/assessments/download-file/${id}`)
+                .send({
+                    password: "wrong-password",
+                })
+                .expect(429);
+
+            expect(response.body.error).toBe(
+                "Too many invalid password attempts, try again later",
+            );
+        });
     });
 
     describe("Testing edit post", () => {});
