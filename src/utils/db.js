3cfa9b908ab51e006aa37140e0ca8149ae63f457
mrx7355608 <mrx7355608@gmail.com>
2024-07-28 15:19:20 +0500

    updated db.js code for serverless environment

--- src/utils/db.js
+++ src/utils/db.js
@@ -1,14 +1,20 @@
 import mongoose from "mongoose";
 
-let cachedConnection = null;
+let cached = global.mongoose;
 
 export async function connectDB(url) {
-    if (cachedConnection) {
-        return;
+    if (cached && cached.conn && cached.conn.readyState === 1) {
+        console.log("Using cached connection");
+        return cached.conn;
     }
 
-    cachedConnection = await mongoose.connect(url);
+    if (cached && cached.conn) {
+        await cached.conn.close();
+    }
+
+    cached.conn = await mongoose.connect(url);
     console.log("Connected to database");
+    return cached.conn;
 }
 
 export async function disconnectDB() {
