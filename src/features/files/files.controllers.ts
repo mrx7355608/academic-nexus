a219b38ca1a5bebc3e29b4edd92b43b6fb4a6722
mrx7355608 <mrx7355608@gmail.com>
2024-09-03 09:07:25 +0500

    separated business logic from controllers to services

--- src/features/files/files.controllers.ts
+++ src/features/files/files.controllers.ts
@@ -1,19 +1,10 @@
 import FilesModel from "./files.model";
 import validator from "validator";
 import ApiError from "../../utils/ApiError";
-import { v2 as cloudinary } from "cloudinary";
 import StudentModel from "../students/students.model";
-import config from "../../config/config";
 import { RequestHandler } from "express";
 import { PipelineStage } from "mongoose";
-import fileValidators from "./files.validators"
 import FileServices from "./files.services";
-
-cloudinary.config({
-    cloud_name: config.cloudinaryCloudName,
-    api_key: config.cloudinaryApiKey,
-    api_secret: config.cloudinaryApiSecret,
-});
 
 const fileServices = FileServices();
 
@@ -98,7 +89,7 @@ const getAllFiles: RequestHandler = async (req, res, next) => {
 
 const getOneFile: RequestHandler = async (req, res, next) => {
     try {
-        const assessment = fileServices.listOne((req as any).assessment)
+        const assessment = fileServices.listOne((req as any).assessment);
         return res.status(200).json({
             ok: true,
             data: assessment,
@@ -111,7 +102,7 @@ const getOneFile: RequestHandler = async (req, res, next) => {
 const createFile: RequestHandler = async (req, res, next) => {
     try {
         const data = req.body;
-        const userId= (req as any).user._id
+        const userId = (req as any).user._id;
         await fileServices.create(data, userId);
 
         return res.status(201).json({
@@ -176,10 +167,11 @@ const createFile: RequestHandler = async (req, res, next) => {
 const editFile: RequestHandler = async (req, res, next) => {
     try {
         const assessment = (req as any).assessment;
-        const assessmentId = (req as any).assessment._id;
         const userId = (req as any).user._id;
         const changes = req.body;
-        const updated = await fileServices.edit(assessmentId, userId, assessment, changes);
+
+        const updated = await fileServices.edit(assessment, userId, changes);
+
         return res.status(200).json({
             ok: true,
             data: updated,
@@ -192,24 +184,8 @@ const editFile: RequestHandler = async (req, res, next) => {
 const deleteFile: RequestHandler = async (req, res, next) => {
     try {
         const assessment = (req as any).assessment;
-
-        // Check if author matches
-        if (String(assessment.author) !== String((req.user as any)._id)) {
-            return next(new ApiError("You cannot delete this assessment", 403));
-        }
-
-        // Delete assessment
-        await FilesModel.findByIdAndDelete(req.params.id);
-
-        // Also delete file from cloudnary
-        cloudinary.api
-            .delete_resources([assessment.publicId], {
-                type: "upload",
-                resource_type:
-                    assessment.fileExtension === "docx" ? "raw" : "image",
-            })
-            .then(console.log);
-
+        const userId = (req.user as any)._id;
+        await fileServices.remove(assessment, userId);
         return res.status(204).end();
     } catch (err) {
         return next(err);
@@ -219,36 +195,11 @@ const deleteFile: RequestHandler = async (req, res, next) => {
 const getMyFiles: RequestHandler = async (req, res, next) => {
     try {
         const { type } = req.params;
-
-        // Validate type
-        const validTypes = [
-            "quiz",
-            "assignment",
-            "report",
-            "proposal",
-            "labtask",
-        ];
-        if (!validTypes.includes(type)) {
-            return next(new ApiError("Invalid type", 400));
-        }
-
-        // Build query
-        let queryFilter = { type };
-
-        // Get subject from query
-        if (req.query.subject) {
-            queryFilter = Object.assign(queryFilter, {
-                subject: req.query.subject,
-            });
-        }
-
-        const assessments = await FilesModel.find(queryFilter).populate(
-            "author",
-            "fullname",
-        );
+        const subject = req.query.subject as string | undefined;
+        const files = await fileServices.listMyFiles(type, subject);
         return res.status(200).json({
             ok: true,
-            data: assessments,
+            data: files,
         });
     } catch (error) {
         next(error);
@@ -258,23 +209,10 @@ const getMyFiles: RequestHandler = async (req, res, next) => {
 const getStudentFiles: RequestHandler = async (req, res, next) => {
     try {
         const { id } = req.params;
-
-        if (!validator.isMongoId(id)) {
-            return next(new ApiError("Invalid mongo id", 400));
-        }
-
-        const student = await StudentModel.findById(id);
-        if (!student) {
-            return next(new ApiError("Student not found", 404));
-        }
-
-        const assessments = await FilesModel.find({ author: id }).populate(
-            "author",
-            "fullname",
-        );
+        const files = await fileServices.listStudentFiles(id);
         return res.status(200).json({
             ok: true,
-            data: assessments,
+            data: files,
         });
     } catch (error) {
         next(error);
--- src/features/files/files.services.ts
+++ src/features/files/files.services.ts
@@ -1,26 +1,37 @@
-import FilesModel from "./files.model"
+import FilesModel from "./files.model";
 import { IFileInput } from "./files.type";
-import fileValidators from "./files.validators"
-import ApiError from "../../utils/ApiError"
+import fileValidators from "./files.validators";
+import ApiError from "../../utils/ApiError";
+import config from "../../config/config";
+import { v2 as cloudinary } from "cloudinary";
+import validator from "validator";
+import StudentModel from "../students/students.model";
+
+cloudinary.config({
+    cloud_name: config.cloudinaryCloudName,
+    api_key: config.cloudinaryApiKey,
+    api_secret: config.cloudinaryApiSecret,
+});
 
 export default function FileServices() {
-    const listAll = async () => {}
+    const listAll = async () => {};
 
     const listOne = async (assessment: any) => {
-        return await assessment.populate(
-            "author",
-            "fullname profilePicture",
-        );
-    }
+        return await assessment.populate("author", "fullname profilePicture");
+    };
 
     const create = async (data: IFileInput, userId: string): Promise<void> => {
         fileValidators.createValidator(data);
         await FilesModel.create({ ...data, author: userId });
-    }
+    };
 
-    const edit = async (assessmentId: string, userId: string, assessment: any, changes: IFileInput) => {
+    const edit = async (
+        assessment: any,
+        userId: string,
+        changes: IFileInput,
+    ) => {
         // Check if author matches
-        if (String(assessment.author) !== userId){
+        if (String(assessment.author) !== userId) {
             throw new ApiError("You cannot edit this assessment", 403);
         }
 
@@ -29,16 +40,82 @@ export default function FileServices() {
 
         // Update assessment
         const updated = await FilesModel.findByIdAndUpdate(
-            assessmentId,
+            assessment._id,
             changes,
             { new: true },
         );
 
         return updated;
-    }
-    const remove = async () => {}
-    const listMyFiles = async () => {}
-    const listStudentFiles = async () => {}
+    };
+
+    const remove = async (assessment: any, userId: string) => {
+        // Check if author matches
+        if (String(assessment.author) !== userId) {
+            throw new ApiError("You cannot delete this assessment", 403);
+        }
+
+        // Delete assessment
+        await FilesModel.findByIdAndDelete(assessment._id);
+
+        // Also delete file from cloudnary
+        cloudinary.api
+            .delete_resources([assessment.publicId], {
+                type: "upload",
+                resource_type:
+                    assessment.fileExtension === "docx" ? "raw" : "image",
+            })
+            .then(console.log);
+    };
+
+    const listMyFiles = async (type: string, subject: string | undefined) => {
+        // Validate type
+        const validTypes = [
+            "quiz",
+            "assignment",
+            "report",
+            "proposal",
+            "labtask",
+        ];
+        if (!validTypes.includes(type)) {
+            throw new ApiError("Invalid type", 400);
+        }
+
+        // Build query
+        let queryFilter = { type };
+
+        // Get subject from query
+        if (subject) {
+            queryFilter = Object.assign(queryFilter, {
+                subject: subject,
+            });
+        }
+
+        // Find files
+        const files = await FilesModel.find(queryFilter).populate(
+            "author",
+            "fullname",
+        );
+
+        return files;
+    };
+
+    const listStudentFiles = async (studentId: string) => {
+        if (!validator.isMongoId(studentId)) {
+            throw new ApiError("Invalid student id", 400);
+        }
+
+        const student = await StudentModel.findById(studentId);
+        if (!student) {
+            throw new ApiError("Student not found", 404);
+        }
+
+        const files = await FilesModel.find({ author: studentId }).populate(
+            "author",
+            "fullname",
+        );
+
+        return files;
+    };
 
     return {
         listAll,
@@ -48,5 +125,5 @@ export default function FileServices() {
         create,
         edit,
         remove,
-    }
+    };
 }
